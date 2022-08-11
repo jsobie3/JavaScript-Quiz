1a7d66af583df6fff03aef08131488f8e9b07c9c
jsobie3 <109123352+jsobie3@users.noreply.github.com>
2022-08-11 18:29:41 -0500

    Add files via upload

---
+++ Assets/script.js
@@ -0,0 +1,47 @@
+var quizQuestions = [
+    {
+    question : " question 1" ,
+    a: "choice a" ,
+    b: "choice b" ,
+    c: "choice c" ,
+    d: "choice d" ,
+    correctAnswer : "a"
+    },
+
+    {
+    question : " question 2" ,
+    a: "choice a" ,
+    b: "choice b" ,
+    c: "choice c" ,
+    d: "choice d" ,
+    correctAnswer : "a"
+    },
+
+    {
+        question : " question 3" ,
+        a: "choice a" ,
+        b: "choice b" ,
+        c: "choice c" ,
+        d: "choice d" ,
+        correctAnswer : "a"
+    },
+
+    {
+        question : " question 4" ,
+        a: "choice a" ,
+        b: "choice b" ,
+        c: "choice c" ,
+        d: "choice d" ,
+        correctAnswer : "a"
+    },
+
+    {
+        question : " question 5" ,
+        a: "choice a" ,
+        b: "choice b" ,
+        c: "choice c" ,
+        d: "choice d" ,
+        correctAnswer : "a"
+    },
+];
+
